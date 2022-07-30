186a074f46475dc5a3e5fee9cf17a44150643b98
Brian Li <bcl2@andrew.cmu.edu>
2022-07-30 17:42:47 -0500

    promise

--- test.py
+++ test.py
@@ -7,3 +7,5 @@ print("solidifying the practice into memory")
 print("and understand how to save to github")
 
 print("just making a couple changes to see if I've got the handle of it")
+
+print("ok promise this is the last check")
