bd194329057e8092f938cbca7581766f26ed76bf
Brian Li <bcl2@andrew.cmu.edu>
2022-07-30 17:41:56 -0500

    one last check

--- test.py
+++ test.py
@@ -5,3 +5,5 @@ print("changes hello")
 print("solidifying the practice into memory")
 
 print("and understand how to save to github")
+
+print("just making a couple changes to see if I've got the handle of it")
