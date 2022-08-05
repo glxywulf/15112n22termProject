986cf3dfaa5153ccf3fc471f3a36b9903bc6a9d8
Brian Li <bcl2@andrew.cmu.edu>
2022-08-05 14:09:53 -0500

    level images whoo; retrying collision

--- Main.py
+++ Main.py
@@ -1,13 +1,14 @@
 from cmu_112_graphics import *
 from Line import *
 from Player import *
+from lvlImages import *
 
 # Model ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def appStarted(app):    
     # window dimensions
-    app.width = 1180
-    app.height = 820
+    app.width = 1200
+    app.height = 850
     
     # make the plaer
     app.player = Player()
@@ -15,9 +16,24 @@ def appStarted(app):
     # keep track of all the lines
     app.lines = []
     
+    app.lines.append(Line(20, 0, 20, 460))
+    app.lines.append(Line(20, 460, 320, 460))
+    app.lines.append(Line(320, 460, 320, 820))
+    app.lines.append(Line(320, 820, 880, 820))
+    app.lines.append(Line(880, 820, 880, 460))
+    app.lines.append(Line(880, 460, 1180, 460))
+    app.lines.append(Line(1180, 460, 1180, 0))
+    app.lines.append(Line(460, 100, 740, 100))
+    app.lines.append(Line(460, 100, 460, 220))
+    app.lines.append(Line(460, 220, 740, 220))
+    app.lines.append(Line(740, 220, 740, 100))
+    
+    app.image1 = app.loadImage('1.png')
+    
+    # * testing lines-------------------------------
     # horizontal pair
-    app.lines.append(Line(0, 100, app.width, 100))
-    app.lines.append(Line(0, app.height - 100, app.width, app.height - 100)) #horiz
+    # app.lines.append(Line(0, 100, app.width, 100))
+    # app.lines.append(Line(0, app.height - 100, app.width, app.height - 100)) #horiz
     
     # vertical pair
     # app.lines.append(Line(200, 0, 200, app.height))
@@ -25,6 +41,7 @@ def appStarted(app):
     
     # diagonal line
     # app.lines.append(Line(0,0,app.width,app.height)) #diag
+    # * testing lines-------------------------------
         
     # set default walk speed for player
     app.timerDelay = 10
@@ -50,16 +67,17 @@ def keyReleased(app, event):
         app.player.setDeltas(0, 0)
         
 def timerFired(app):
-    app.player.checkCollision(app.lines)
+    app.player.checkCollisions(app.lines)
     app.player.movePlayer()
     app.player.applyGravity()
     
 # View ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def redrawAll(app, canvas):
+    canvas.create_image(0, 0, image = ImageTK.PhotoImage(app.image1))
     for i in range(len(app.lines)):
         app.lines[i].drawLine(app, canvas)
     app.player.drawPlayer(app, canvas)
     
     
-runApp(width = 1180, height = 820)
+runApp(width = 1200, height = 850)
