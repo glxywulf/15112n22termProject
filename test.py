5c6e5099374921553fa9ab5f8ca26ec2b14541c4
Brian Li <bcl2@andrew.cmu.edu>
2022-08-05 14:11:45 -0500

    images?

--- test.py
+++ test.py
@@ -148,3 +148,249 @@
 #             return [True, interX, interY]
         
 #         return [False, 0, 0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def checkCollision(self, lines):
+    #     colLines = []
+        
+    #     for line in lines:
+    #         if(self.isColliding(line)):
+    #             colLines.append(line)
+                
+    #     landing = False
+    
+    # def isColliding(self, line): # ? check code bullets IsCollidingWithLine function in Player class
+    #     # if the line is horizontal
+    #     if(line.isHorizontal):
+    #         # check if the player is close to the line's x and y position
+    #         isPlayerCloseX = ((line.x1 < self.cx and self.cx < line.x2) or 
+    #                           (line.x1 < self.cx + self.width and self.cx + self.width < line.x2) or 
+    #                           (self.cx < line.x1 and line.x1 < self.cx + self.width) or 
+    #                           (self.cx < line.x2 and line.x2 < self.cx + self.width))
+    #         isPlayerCloseY = self.cy < line.y1 and line.y1 < self.cy + self.height
+            
+    #         return (isPlayerCloseX and isPlayerCloseY)
+        
+    #     # if the line is vertical
+    #     elif(line.isVertical):
+    #         # check if the player is close to the line's x and y position
+    #         isPlayerCloseY = ((line.y1 < self.cy and self.cy < line.y2) or 
+    #                           (line.y1 < self.cy + self.height and self.cy + self.height < line.y2) or 
+    #                           (self.cy < line.y1 and line.y1 < self.cy + self.height) or 
+    #                           (self.cy < line.y2 and line.y2 < self.cy + self.height))
+    #         isPlayerCloseX = self.cx < line.x1 and line.x1 < self.cx + self.width
+            
+    #         return (isPlayerCloseX and isPlayerCloseY)
+
+    #     # if the line is diagonal
+    #     else:
+    #         # line coords
+    #         x1 = line.x1
+    #         y1 = line.y1
+    #         x2 = line.x2
+    #         y2 = line.y2
+            
+    #         # top left corner coords
+    #         topLeftX = self.cx
+    #         topLeftY = self.cy
+
+    #         # top right corner coords
+    #         topRightX = self.cx + self.width
+    #         topRightY = self.cy
+
+    #         # bottom left corner coords
+    #         botLeftX = self.cx
+    #         botLeftY = self.cy + self.height
+
+    #         # bottom right corner coords
+    #         botRightX = self.cx + self.width
+    #         botRightY = self.cy + self.height
+            
+    #         # check line collisions of player bottom
+    #         left = checkLineLine(topLeftX, topLeftY, botLeftX, botLeftY, x1, y1, x2, y2)
+
+    #         # check line collisions of player bottom
+    #         right = checkLineLine(topRightX, topRightY, botRightX, botRightY, x1, y1, x2, y2)
+
+    #         # check line collisions of player bottom
+    #         top = checkLineLine(topLeftX, topLeftY, topRightX, topRightY, x1, y1, x2, y2)
+
+    #         # check line collisions of player bottom
+    #         bottom = checkLineLine(bottomLeftX, bottomLeftY, bottomRightX, bottomRightY, x1, y1, x2, y2)
+            
+            
+    #         if(left[0] or right[0] or top[0] or bottom[0]):
+    #             # create a new set of collision information
+    #             lineCollisInfo = Collision()
+                
+    #             # set the variables to true if whatever diagonal line has collided with the player
+    #             lineCollisInfo.collidedLeft = left[0]
+    #             lineCollisInfo.collidedRight = right[0]
+    #             lineCollisInfo.collidedTop = top[0]
+    #             lineCollisInfo.collidedBottom = bottom[0]
+                
+    #             # add a tuple of the coordinates that intersected with the line to
+    #             # the list inside of the Collision object
+    #             if(left[0]):
+    #                 lineCollisInfo.collidedPoints.append((left[1], left[2]))
+    #             if(right[0]):
+    #                 lineCollisInfo.collidedPoints.append((right[1], right[2]))
+    #             if(top[0]):
+    #                 lineCollisInfo.collidedPoints.append((top[1], top[2]))
+    #             if(bottom[0]):
+    #                 lineCollisInfo.collidedPoints.append((bottom[1], bottom[2]))
+                
+    #             # set the inputted line's collision object to whatever we have just
+    #             # newly made
+    #             line.collisions = lineCollisInfo
+                
+    #             return True
+            
+    #         else:
+    #             return False
+
+    # # in the case we collide with two lines, we have to check which one we
+    # # should prioritize when it comes to landing or bouncing off of it
+    # # TODO prolly cite stuff 
+    # def getPriority(self, lines):
+    #     # if there are no lines we're colliding against we don't need to return
+    #     # anything since there would be nothing to return
+    #     if(len(lines) == 0): return None
+        
+    #     # we should only care if we come into contact with exactly two lines
+    #     elif(len(lines) == 2):
+    #         # set variables to None and assign them later to get orientations
+    #         # of the 2 lines that we collided with
+    #         vertical = None
+    #         horizontal = None
+    #         diagonal = None
+            
+    #         # now we check if either line in lines is vert/hori/diag and set
+    #         # the above variables to be that line
+    #         if(lines[0].isVertical):
+    #             vertical = lines[0]
+    #         if(lines[0].isHorizontal):
+    #             horizontal = lines[0]
+    #         if(lines[0].isDiagonal):
+    #             diagonal = lines[0]
+    #         if(lines[1].isVertical):
+    #             vertical = lines[1]
+    #         if(lines[1].isHorizontal):
+    #             horizontal = lines[1]
+    #         if(lines[1].isDiagonal):
+    #             diagonal = lines[1]
+                
+    #         # now we have to check each possible case that the pair of lines
+    #         # could be; vert/hori, hori/diag, and diag/vert. although diag/vert
+    #         # doesn't actually have any impact in the real game so we'll ignore
+    #         # it for now
+    #         if((vertical != None) and (horizontal != None)):
+    #             # calculate the mid Ys of each line so we can compare them later
+    #             # technically the horizontal midline calculation is unnecessary
+    #             # but I want to be consistent
+    #             vertYMid = (vertical.y1 + vertical.y2) / 2
+    #             horiYMid = (horizontal.y1 + horizontal.y2) / 2
+                
+    #             # if the player is moving up
+    #             if(self.dy < 0):
+    #                 # if the midY of the vertical line is greater when the 
+    #                 # player is moving up, then priority goes to the vertical line
+    #                 # this would be a top right corner when imagined
+    #                 if(vertYMid > horiYMid):
+    #                     return vertical
+                    
+    #                 # otherwise, priority goes to the horizontal line
+    #                 else:
+    #                     return horizontal
+                    
+    #             # in the case that the player is moving down
+    #             else:
+    #                 # if the midY of the vertical line is below the y of the
+    #                 # horizontal line, then the vertical line gets priority
+    #                 # this is a top left corner when imagined
+    #                 if(vertYMid < horiYMid):
+    #                     return vertical
+                    
+    #                 # horizontal gets priority otherwise
+    #                 else:
+    #                     return horizontal
+            
+    #         # if the pair of lines are horizontal and diagonal
+    #         if((horizontal != None) and (diagonal != None)):
+    #             horiYMid = (horizontal.y1 + horizontal.y2) / 2
+    #             diagYMid = (diagonal.y1 + diagonal.y2) / 2
+                
+    #             # if the diagonal line's midY is below the horizontal, we want
+    #             # to prioritize the horizontal line. since we've techinically 
+    #             # landed on the horizontal already
+    #             if(horiYmid < diagYmid):
+    #                 return horizontal
+                
+    #             # diagonal gets priority otherwise
+    #             else:
+    #                 return diagonal
+                
+    #     # now we have to correct
+    #     # ? maybe come back to this.  
+    #     elif(len(lines) > 2):
+    #         for line in lines:
+    #             directedCorrectX = 0
+    #             directedCorrectY = 0
+    #             minCorrect = 100000
+    #             correction = None
+                
+    #             if(line.isHorizontal):
+    #                 if(self.dy < 0):
+    #                     directedCorrectY = line.y1 - (self.cy + self.height)
+    #                     correction = abs(self.cy - (line.y1 - self.height))
+    #                 else:
+    #                     directedCorrectY = line.y1 - self.cy
+    #                     correction = abs(self.cy - line.y1)
+                        
+    #             elif(line.isVertical):
+    #                 if(self.dx < 0):
+    #                     directedCorrectX = line.x1 - self.cx
+    #                     correction = abs(self.cx - line.x1)
+    #                 else:
+    #                     directedCorrectX = line.x1 - (self.cx + self.width)
+    #                     correction = abs(self.cx - (line.x1 - self.width))
+    #             else:
+    #                 if(len(line.collisions.collidedPoints) == 2):
+    #                     pass
+            
+    #         # ! line 987 of player class
+
+    # # function to check if lines are colliding with each other based on points
+    # # of lines and points of player using some math formulas
+    # # TODO paste citation stuff
+    # def checkLineLine(self, x1, y1, x2, y2, x3, y3, x4, y4):  
+    #     uA = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))      
+    #     uB = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
+        
+    #     # if uA and uB are within 0-1 inclusive, then the pair of lines are colliding
+    #     if((uA >= 0 and uA <= 1) and (uB >= 0 and uB <= 1)):
+    #         interX = x1 + (uA * (x2 - x1))
+    #         interY = y1 + (uA * (y2 - y1))
+            
+    #         # return True and the point at which there is an intersection
+    #         return [True, interX, interY]
+        
+    #     # return false otherwise
+    #     return [False, 0, 0]
+    
+    
+    
+import sys
+print(f'"{sys.executable}" -m pip install pillow')
+print(f'"{sys.executable}" -m pip install requests')
